ef5c159a72ace4a5ae63a55cbd02cb22f25117db
h662 <h662hong@gmail.com>
2021-08-15 18:59:07 +0900

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
 
 const App: FC = () => {
-  return <div className="bg-red-100">App</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex">
+      <div className="bg-blue-100 flex-auto">1</div>
+      <div className="bg-purple-300 max-w-screen-sm flex-auto">2</div>
+      <div className="bg-blue-100 flex-auto">3</div>
+    </div>
+  );
 };
 
 export default App;
